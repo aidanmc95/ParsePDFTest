321f7e0d66a4f95d58c3585652199391b12783bd
aidanmc95 <aidanmc95@gmail.com>
2020-10-23 21:09:49 -0700

    Switch to PyPDF2

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
+import sys
+import os
+
 from pdfrw import PdfReader
+import PyPDF2  
+    
+# creating a pdf file object  
+pdfFileObj = open('test.pdf', 'rb')  
+    
+# creating a pdf reader object  
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)  
+    
+# printing number of pages in pdf file  
+print(pdfReader.numPages)  
+    
+# creating a page object  
+pageObj = pdfReader.getPage(0)  
+    
+# extracting text from page  
+print(pageObj.extractText())  
+    
+# closing the pdf file object  
+pdfFileObj.close() 
 
 if __name__ == "__main__":
-    
+
+    # # inpfn, = sys.argv[1:]
+    # # outfn = 'alter.' + os.path.basename(inpfn)
+
+    # doc = PdfReader('test.pdf')
+    # for page in doc.pages:
+    #     bytestream = page.Contents.stream
+    #     print(utf8_decode())
+    # # trailer.Info.Title = 'My New Title Goes Here'
+    # # PdfWriter(outfn, trailer=trailer).write()
+    print("hi")
